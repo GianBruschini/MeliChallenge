fd0cb253dac90ebad3153c971d2f8051de85bd32
Gianfranco Bruschini <59579790+Hanzek@users.noreply.github.com>
2024-01-15 09:03:48 -0300

    fix(ProductRepositoryTest): method names modified

--- app/src/main/java/com/meli/melichallenge/util/BundleKeys.kt
+++ app/src/main/java/com/meli/melichallenge/util/BundleKeys.kt
@@ -3,9 +3,7 @@ package com.meli.melichallenge.util
 class BundleKeys {
     companion object {
         const val PRODUCT_ID = "productId"
-        const val PRODUCT_TITLE = "productTitle"
         const val PRODUCT_NAME = "productName"
-        const val PRODUCT_IMAGE = "productImage"
         const val PRODUCT_PERMALINK = "productPermalink"
     }
 }
